b070410f46af49e33024c52251bc6e6bd4dee840
renanpmiranda <renanpmiranda5@gmail.com>
2022-09-08 15:59:55 -0300

    exercicios_a012

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,13 @@
-const num = //seu numero aqui
+const num = Number(prompt('Forneça um número:'))
 
+if(num % 2 === 0){
+    if(num % 3 === 0){
+        console.log('É divisível por 2 ou por 3.')
+    }
+}
+
+if(num % 2 === 0 || num % 3 === 0){
+    console.log('É divisível por 2 ou por 3.')
+}else{
+    console.log('Não é divisível por 2 ou por 3.')
+}
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,20 @@
-//código a ser reescrito
+const nome = prompt('Qual o seu nome?')
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+nome === 'José' ? console.log('Oi, Zé!') : console.log('Olá, '+nome)
+	
+const idade = Number(prompt('Qual a sua idade?'))
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+idade >= 18 ? console.log('pode tirar carteira de motorista!') : console.log('Ainda não pode tirar carteira de motorista')
+
+// código a ser reescrito:
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
+
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
